631d1f57c7eeac17fb2c514fd3e689bda28af60a
Ian Macalinao <github@igm.pub>
2025-07-15 12:26:40 +0800

    fix deprecated

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,13 @@
  */
 
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), TanStackRouterVite(), react(), tailwindcss()],
-  server: {
-    port: 3000,
-    host: true,
-  },
+  plugins: [tsconfigPaths(), tanstackRouter(), react(), tailwindcss()],
   build: {
     target: "esnext",
     outDir: "dist",
